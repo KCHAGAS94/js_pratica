3b53f82539f4bae2d12e549a8073309f17422457
kenedy <kenedychagas@gmail.com>
2025-11-27 21:02:26 -0300

    desafio2

---
+++ templateLiterais.js
@@ -0,0 +1,11 @@
+
+const firstName = "Kenedy";
+
+const email = 'Hello ' + firstName + ', welcome to our site!';
+
+const email2 = 
+`Hello ${firstName}, 
+welcome to our site!`;
+
+console.log(email);
+console.log(email2);
